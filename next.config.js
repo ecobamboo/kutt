fe22e8133c0755873dd1f019b66443a46a01a335
stepannemejc <stepannemejc@users.noreply.github.com>
2023-11-21 15:03:19 +0100

    redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://ecobamboo.cz',
+        permanent: true,
+      },
+    ]
+  },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
